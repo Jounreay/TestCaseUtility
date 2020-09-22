42000d26eeb37da258973b9e00bcd202976f06e8
Jounreay <joshua-reid@raider.rose.edu>
2020-09-21 22:05:08 -0500

    refactored using ttk button instead of tkbutton

--- GUI.py
+++ GUI.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import TestCaseUtility as tcu
+from tkinter.ttk import *
 
 
 class TestcaseUtilityMain(tk.Frame):
@@ -10,21 +11,22 @@ class TestcaseUtilityMain(tk.Frame):
         self.create_widget()
         self.open_widget()
         self.exit_widget()
+        tk.Frame(root).config(highlightthickness=2, padx=2)
         root.geometry("250x116")
         root.option_add("*Button.Background", "black")
         root.option_add("*Button.Foreground", "green")
         root["bg"] = "#aaaaaa"
 
     def create_widget(self):
-        create_button = tk.Button(master=root, text="Create", command=self.create_file)
+        create_button = Button(master=root, text="Create", style='TButton', command=self.create_file)
         create_button.pack(side="left")
 
     def open_widget(self):
-        open_button = tk.Button(master=root, text="Open", command=self.open_file)
+        open_button = Button(master=root, text="Open", style='TButton', command=self.open_file)
         open_button.pack(side="right")
 
     def exit_widget(self):
-        quit_button = tk.Button(text="QUIT", fg="red", command=self.master.destroy)
+        quit_button = Button(text="Exit", command=self.master.destroy)
         quit_button.pack(side="bottom")
 
     def create_file(self):
@@ -36,4 +38,6 @@ class TestcaseUtilityMain(tk.Frame):
 
 root = tk.Tk()
 app = TestcaseUtilityMain(master=root)
+style = Style()
+style.configure('TButton', font=('calibri', 10, 'bold'), borderwidth='4')
 app.mainloop()
